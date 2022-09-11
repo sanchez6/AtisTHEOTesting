9e6727b5f1ee41a22cba7f2d6472a600fa624319
sanchez6 <sanchez6@gmail.com>
2022-09-11 14:59:57 +0200

    Agregar archivos de proyecto.

---
+++ ArtisTHEOMessageLib/Assessment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisTHEOMessageLib
+{
+    public class Assessment : IMessage
+    {
+        private string TextoValoracion;
+        public Assessment(string TextoValoracion)
+        {
+            this.TextoValoracion = TextoValoracion;
+        }
+        public string GetMessageText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRoot()
+        {
+            return true;
+        }
+    }
+}
---
+++ AtisTHEOTesting/ComentariosUT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtisTHEOMessageLib;
+
+namespace AtisTHEOTesting
+{
+    public class ComentariosUT
+    {
+        [Fact]
+        public void TestMetodo1()
+        {
+            //Arrange
+            string TextoValoracion = "Este es el mensaje de prueba de mi primera valoración";
+
+            //Act
+            Assessment valoracion = new Assessment(TextoValoracion);
+
+            //Assert
+            Assert.Equal(TextoValoracion, valoracion.GetMessageText());
+
+        }
+    }
+}
